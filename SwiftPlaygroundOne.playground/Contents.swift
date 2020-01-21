5fbc1f5173c91b5f047752d501c54d54bcc4fb23
ocorpuz <59846122+ocorpuz@users.noreply.github.com>
2020-01-21 10:30:04 -1000

    Completed parts 1-7

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
 var str = "Hello, playground"
+//var myFirstVariable: String
+var myFirstVariable = "I've created my first string"
+var mySecondVariable = " and look I added 2 string together"
+myFirstVariable + mySecondVariable
+var myThirdVariable = myFirstVariable + mySecondVariable
+print(myThirdVariable)
+var myFourthVariable = myFirstVariable + mySecondVariable + myThirdVariable
